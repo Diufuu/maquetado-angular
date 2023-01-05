1ef5a7dbe1b0c920b760f3e50346e52438627ffe
Diufuu <109174521+Diufuu@users.noreply.github.com>
2023-01-05 14:53:04 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HttpClientModule } from "@angular/common/http";
+import { BannerComponent } from './banner/banner.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AcademicComponent } from './academic/academic.component';
+import { CapacityComponent } from './capacity/capacity.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BannerComponent,
+    AboutMeComponent,
+    AcademicComponent,
+    CapacityComponent,
+    ProjectsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
